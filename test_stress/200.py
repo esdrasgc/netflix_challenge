561afb4682bad8f07340754bf75e0fff5da49a92
esdrasgc <esdrasgcdev@gmail.com>
2023-04-19 16:50:32 -0300

    test_stress

---
+++ test_stress/200.py
@@ -0,0 +1,43 @@
+from scipy.linalg import svd, diagsvd
+import pandas as pd
+import numpy as np
+
+def selecionar_K_primeiros_valores(u, s, vt, K):
+    u_ = u[:, 0:K]
+    s_ = s[:K]
+    vt_ = vt[:K, :]
+    return u_, s_, vt_
+
+df = pd.read_csv('ratings_small.csv')
+df = df.drop('timestamp', axis=1)
+matrix = pd.pivot_table(df, values='rating', index='userId', columns='movieId')
+
+
+for iteração in range(2):
+    b = matrix.copy()
+    cont = 0
+    indices = set()
+    i = np.random.randint(0, b.shape[0])
+    j = np.random.randint(0, b.shape[1])
+    while cont < 200:
+        if np.isnan(b.iloc[i, j]):
+            i = np.random.randint(0, b.shape[0])
+            j = np.random.randint(0, b.shape[1])
+        else:
+            cont+=1
+            indices.add((i, j))
+
+    b.iloc[i, j] = np.random.randint(5, 51) / 10
+    b = b.fillna(2.75)
+
+    b = b.to_numpy()
+
+    U, s, Vt = svd(b)
+
+    U_, s_, Vt_ = selecionar_K_primeiros_valores(U, s, Vt, 20)
+
+    B_ = U_ @ diagsvd(s_, U_.shape[1], Vt_.shape[0]) @ Vt_
+
+    diferença = matrix.iloc[i, j] - B_[i, j]
+    with open('test_stress/diferenças_200.txt', 'a') as f:
+        f.write(f'{diferença}\n')
